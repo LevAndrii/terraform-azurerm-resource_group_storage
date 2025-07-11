4b17a8c38031edfadd210d5ef60bd189ab79ae6c
Andrii Levchenko <levchenkoandrii11@gmail.com>
2025-07-11 14:07:39 +0300

    fixed

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "rg_name" {
-    type = string
-    description = "Name of the resource group"
+  type        = string
+  description = "Name of the resource group"
 }
 
 variable "rg_location" {
-    type = string
-    description = "Location for the resource group"
+  type        = string
+  description = "Location for the resource group"
 }
 
 variable "storage_account_name" {
-    type = string
-    description = "Storage account name"
+  type        = string
+  description = "Storage account name"
 }
