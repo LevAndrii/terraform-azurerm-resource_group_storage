eb1ba9d93b2ea3f740510663e1806569a6951aab
Andrii Levchenko <levchenkoandrii11@gmail.com>
2025-07-11 17:27:53 +0300

    a despearate attempt  make terraform accept the  module

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "example" {
   name     = var.rg_name
   location = var.rg_location
